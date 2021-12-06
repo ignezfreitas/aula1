97cb94edbcd23d35a5012a5d01c4346ee2ccf940
ignezfreitas <ignez.freitas@yahoo.com>
2021-12-06 20:47:09 -0300

    Versão 0.0.1

---
+++ olamundo.php
@@ -0,0 +1,11 @@
+<?php
+//exibe msg no navegador
+echo "olá Mundo - Me chamo Ignez";
+
+//java usar document.write('Olá Mundo)
+
+//JAVA - System.out.printLn("Olá Mundo)
+
+//C - cout>>"olá mundo"
+
+
